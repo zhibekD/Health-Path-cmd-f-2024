99835f9f33c11b12e8c7f22f5f39ffb47609393f
carissaor <carissaor@gmail.com>
2024-03-09 14:12:15 -0800

    searchBar componet

---
+++ frontend/component/searchBar.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { Searchbar } from 'react-native-paper';
+
+const MySearchBar = () => {
+    const [search, setSearch] = useState('');
+  
+    const updateSearch = (search) => {
+      setSearch(search);
+    };
+  
+    return (
+      <Searchbar
+        placeholder="Search"
+        onChangeText={updateSearch}
+        value={search}
+        style = {{ width: '90%' }}
+      />
+    );
+  };
+
+export default MySearchBar;
