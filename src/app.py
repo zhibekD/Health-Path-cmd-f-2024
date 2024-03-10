b72469cc755bb3897bdaae5fc0ff8be098e8248a
Zhibek D <d26zhibek@gmail.com>
2024-03-09 16:32:52 -0800

    added new key

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
 ## Setting up Cohere
 
-import cohere  
-co = cohere.Client(lcybM3ukVgb9iGj6PIvidVJuaeUypRk2gGZLnFc9)
+import cohere
+co = cohere.Client('KJ8iAaWa0pnbcWgxwfPjAE6HrYz5llZTMQ63wDpG')
 
+# Example of using a Cohere API method
+response = co.summarize(
+    text = 'text')
 
-
+print(response)
