b96cbfef8a181ae95dead8417ea76f97edd00699
Zhibek D <d26zhibek@gmail.com>
2024-03-10 01:19:23 -0800

    cohere summary

--- src/app.py
+++ src/app.py
@@ -1,10 +1,73 @@
 import cohere
-from cohere.responses.classify import Example
+import sqlite3
+import os
+
 co = cohere.Client('KJ8iAaWa0pnbcWgxwfPjAE6HrYz5llZTMQ63wDpG')
-classifications = co.classify(
-  model='embed-english-v2.0',
-  inputs=["This item was broken when it arrived", "This item broke after 3 weeks"],
-  examples=[Example("The order came 5 days early", "positive"), Example("The item exceeded my expectations", "positive"), Example("I ordered more for my friends", "positive"), Example("I would buy this again", "positive"), Example("I would recommend this to others", "positive"), Example("The package was damaged", "negative"), Example("The order is 5 days late", "negative"), Example("The order was incorrect", "negative"), Example("I want to return my item", "negative"), Example("The item\'s material feels low quality", "negative")])
-print('The confidence levels of the labels are: {}'.format(
-       classifications.classifications))
+src_dir = os.path.dirname('src')
+database_file = os.path.join(src_dir,'local_hospitales.sqlite')
+
+# retrieve the reviews and summarize them
+# conn = sqlite3.connect('local_hospitales.sqlite')
+# cursor = conn.cursor()
+
+# # Select reviews from the database
+# cursor.execute("SELECT reviews FROM hospitales")
+# reviews_data = cursor.fetchall()
+
+# # Extract reviews from the result
+# reviews = [row[0] for row in reviews_data]
+
+# # Combine all reviews into a single string
+# all_reviews = ' '.join(reviews)
+
+
+# response = co.summarize(
+#     text=all_reviews,
+#     model='command',
+#     length='medium',
+#     extractiveness='medium'
+# )
+
+# summary = response.summary
+
+# print(summary)
+
+# conn.close()
+
+def loadreviewsfromdatabase(databasefile):
+    print("Current working directory:", os.getcwd())
+    conn = sqlite3.connect(databasefile)
+    cursor = conn.cursor()
+    cursor.execute("SELECT reviews FROM hospitales")
+    reviews = cursor.fetchall()
+    conn.close()
+    return reviews
+
+def process_reviews(reviews):
+    for review in reviews:
+        summarize(review[0])
+
+
+def summarize(review):
+    response= co.summarize(
+        text=review,
+        model='command',
+        length='medium',
+        extractiveness='medium'
+    )
+    summary = response.summary
+    print("Summary:", summary)
+    print(summary)
+
+
+r = loadreviewsfromdatabase(database_file)
+
+process_reviews(r)
+
+co.close()
+
+
+
+
+
 
